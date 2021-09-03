6eaa9c8ffd9b1286e681cd4d71b1cb91dc4b6365
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-03 09:31:14 +0530

    escape sequence

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
+//data type and variable
 int main()
 {
-    int a, b;
-    printf("enter  number a:\n ");
-    scanf("&d", &a);
-    printf("enter number b:\n ");
-    scanf("&d", &a);
-
+    printf("\"A\"\tB\n");
+    printf("c\nd\n");
+    printf("a\b b\n");
+    printf("\? \n \'\n \\");
     return 0;
 }
