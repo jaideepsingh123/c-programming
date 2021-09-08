16b80761b6d6da98f4847983ba81f1a44c688e47
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-08 13:10:45 +0530

    logical operator

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-//relational opreators (compare couple of value )(==,!=(not equal to),>(greater then),<(less then)>=,<=)
-
+//logical operators(a&&B) is false,(a||b)is true !(a && b) is true
 int main()
 
 {
-    int a = 20;
-    int b = 20;
     int c;
-    c = a == b;
-    printf("==:%d\n", c);
-    c = a != b;
-    printf("!=:%d\n", c);
-    c = a >= b;
-    printf(">=:%d\n", c);
-    c = a <= b;
-    printf("<=:%d\n", c);
-    c = a > b;
-    printf(">:%d\n", c);
-    c = a < b;
-    printf("<:%d", c);
+    c = 2 || 1;
+    printf("1 || 1:%d\n", !c);
+    c = 3 || 0;
+    printf("1 || 0:%d\n", !c);
+
+    c = 4 || 1;
+    printf("0 || 1:%d\n", !c);
+
+    c = 5 || 0;
+    printf("0 || 0:%d\n", !c);
+
     return 0;
 }
