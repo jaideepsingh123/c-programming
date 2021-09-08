bd4ee7f71d1c013bdba4c9e4325b96e5fb335981
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-08 10:02:22 +0530

    arithmetic operators

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-//type casting(type_name) expression,to convert one data types to another data types
+//arithmetic operators
 int main()
 
 {
-    int sum = 17, count = 5;
-    float meam;
-    meam = (float)sum / count;
-    printf("value of sum:%f\n", meam);
+    int a = 21;
+    int b = 10;
+    int c;
+    c = a + b;
+    printf("line 1:%d\n", c);
+    c = a - b;
+    printf("line 2:%d\n", c);
+    c = a * b;
+    printf("line 3:%d\n", c);
+    c = a / b;
+    printf("line 4:%d\n", c);
+    c = a % b;
+    printf("line 5 :%d\n", c);
+    c = a++; /*increasing*/
+    printf("line 6:%d\n", c);
+    c = a--; /*decreasing*/
+    printf("line 7:%d\n", c);
+    c = ++a;
+    printf("line 8:%d\n", c);
+    c = --a;
+    printf("line 9:%d\n", c);
 
     return 0;
 }
