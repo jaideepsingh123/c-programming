3ee7a2b8387094e32acf758cc99b13bd29909cdb
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-09 10:22:42 +0530

    if statement

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-//operators precedence(determines the grouping of terms in an expression)
+//if statement
 int main()
 {
-    int result;
-    result = 5 + 4 / 2;
-    printf("result:%d\n", result);
-
-    result = 5 + 2 - 3;
-    printf("result:%d\n", result);
-
-    result = 5 - 2 + 3;
-    printf("result:%d\n", result);
-    return 0;
+    int num;
+    printf("enter your number:");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        printf("welcome to this beautifull world of operators");
+    }
 }
