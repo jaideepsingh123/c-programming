6ed63220d05efec6aad0a7241a066eefc05ad0e1
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-10 10:36:48 +0530

    for loop - corrected

--- main.c
+++ main.c
@@ -5,8 +5,9 @@ int main()
     int i, sum = 0;
     for (i = 10; i >= 1; i--)
     {
-        printf("i=%d\n", sum);
+        printf("i=%d\n", i);
         sum = sum + i;
+        printf("sum:%d\n", sum);
     }
 
     return 0;
