3218200ced35f6834049ac945d9001418ffe65a4
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-10 10:13:57 +0530

    for loop

--- main.c
+++ main.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
-//switch statement
+//for loop
 int main()
 {
-    char ch;
-    printf("enter a character:");
-    scanf("%c", &ch);
-
-    switch (ch)
+    int i;
+    for (i = 1; i <= 10; i++)
     {
-    case 'a':
-        printf("a is a vowel");
-        break;
-
-    case 'e':
-        printf("e is a vowel");
-        break;
-    case 'i':
-        printf("i is vowel");
-        break;
-    case 'o':
-        printf("o is vowel");
-        break;
-    case 'u':
-        printf("u is vowel");
-        break;
-    default:
-        printf("consonant / special character");
-        break;
+        printf("i=%d\n", i);
     }
 
     return 0;
