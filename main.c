c0b94b93427a1c8df30dab144146ecd5884f4681
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-10 13:39:34 +0530

    while loop

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-//for loop(repeat line according to our need)
+//while loop(executes statement as long as a given condition is true)
 int main()
 {
-    int i, sum = 0;
-    for (i = 10; i >= 1; i--)
-    {
-        printf("i=%d\n", i);
-        sum = sum + i;
-        printf("sum:%d\n", sum);
-    }
+    int num, num1, count = 0;
+    printf("enter a number :");
+    scanf("%d", &num);
 
+    num1 = num;
+    while (num1 != 0)
+    {
+        num1 = num1 / 10; //4562 divide 10 and point will covert zero to one (4567)-456-45-4
+        count++;          //0-1-2-3-4
+    }
+    printf("%d is %d digit number .", num, count);
     return 0;
 }
