18a6b901031ac291ed47608aef397c5fa779fc91
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-13 13:02:27 +0530

    arrays

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
-//how to print square roots
-
+//arrays(defined as collection of similar data items under single name),store squentional form ([1],[2])
 int main()
 {
-    char ch = 'a';
-    while (ch <= 'z')
+    int arr[5] = {43, 56, 21, 56, 78};
+    int i, j, temp = 0;
+    printf("before sorting\n ");
+    for (i = 0; i < 5; i++)
     {
-        printf("%c\n", ch);
-        ch++;
+        printf("%d:%d\n", i, arr[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 4; i++)
+        {
+            if (arr[j] > arr[j + i])
+            {
+                arr[j] ^= arr[j + i];
+                arr[j + i] ^= arr[j];
+                arr[j] ^= arr[j + i];
+            }
+        }
+    }
+    printf("\n\nAfter sorting\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d:%d\n", i, arr[i]);
     }
     return 0;
 }
