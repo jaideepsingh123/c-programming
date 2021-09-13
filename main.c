11121613e4dec080de98b4a5d436d1ec1df4e37b
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-13 09:55:14 +0530

    while loop -question

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-//while loop(executes statement as long as a given condition is true)
 int main()
 {
-    int num, num1, count = 0;
-    printf("enter a number :");
-    scanf("%d", &num);
+    int i, end;
+    printf("print all square root from 1 to :");
+    scanf("%d", &end);
 
-    num1 = num;
-    while (num1 != 0)
+    i = 1;
+    while (i <= end)
     {
-        num1 = num1 / 10; //4562 divide 10 and point will covert zero to one (4567)-456-45-4
-        count++;          //0-1-2-3-4
+        printf("%d\n", i * i * i);
+        i++;
     }
-    printf("%d is %d digit number .", num, count);
     return 0;
 }
