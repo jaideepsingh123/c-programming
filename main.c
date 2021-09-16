5f6514a7d02e7176af27a5ceea3cefda6e650858
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-16 10:45:19 +0530

    string

--- main.c
+++ main.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-//arrays(defined as collection of similar data items under single name),store squentional form ([1],[2])
+
+//string is actually is one dimensional array of character terminated by null charactor ("\0") %S for calling strings
 int main()
 {
-    int arr[5] = {43, 56, 21, 56, 78};
-    int i, j, temp = 0;
-    printf("before sorting\n ");
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d:%d\n", i, arr[i]);
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 4; i++)
-        {
-            if (arr[j] > arr[j + i])
-            {
-                arr[j] ^= arr[j + i];
-                arr[j + i] ^= arr[j];
-                arr[j] ^= arr[j + i];
-            }
-        }
-    }
-    printf("\n\nAfter sorting\n");
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d:%d\n", i, arr[i]);
-    }
+    char greeting[10] = {'h', 'e', 'l', 'l', 'o', '\0'};
+    printf("%c\n", greeting[2]);
+    printf("%s\n", greeting);
+    printf("\nenter a greeting message 1:");
+    scanf("%[^\n]s", greeting);
+
+    printf("\ngreeting message :%s", greeting);
+
+    fflush(stdin);
+
+    printf("\n enter a greeting message 2:");
+    scanf("%[^\n]s", greeting);
+
+    printf("\ngreeting message :%s", greeting);
     return 0;
 }
