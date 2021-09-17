0051154a5c44d353bf79de6c041e89ba4e2bcae0
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-17 10:25:04 +0530

    storage classes

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-//pointer  to pointer is a form f multiple indirection or a chain of pointers
-int main()
+//storage classes defines the scope (visibility) and life time of variable
+
+/*auto(suppose a local variable to convert it storage classes)*/
+
+/*examples of auto classes*/
 {
-    int i = 10;
-    int *ip = &i;
-    int **iip = &ip;
-    printf("Address of i:%x\n\n\n\n", &i);
-    printf("value of ip:%x\n\n", ip);
-    printf("value at address of iip:%X\n\n\n\n", *iip);
-    printf("value of i:%d\n\n", i);
-    printf("value of i:%d\n\n", *ip);
-    printf("value of i:%d\n\n\n", **iip);
+    int mount auto int month;
 }
+/*auto storage class is the default storage class for all local variable*/
