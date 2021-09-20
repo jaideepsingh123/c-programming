f99f34a6b54cc3ce060fadcbab9d7446e13cc74f
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-20 12:05:35 +0530

    union

--- main.c
+++ main.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//structure provide provision of combining several types of data items under a single entity represent data
-//entity is a object use to identify units
-struct emp
+//union is a data type enable to store different data type in the same memory location also provide efficent way of using same memory for multi-purpose
+union id
 {
-    int id;
-    char name[10];
-    char designation[10];
+    char drivinglicense[10];
+    char passport[15];
 };
-typedef struct emp employee;
+typedef union id identify;
 int main()
-
 {
-    employee e1; /*e1 is even no  of variable to employee*/
-    e1.id = 100;
-    strcpy(e1.name, "jaideep");
-    strcpy(e1.designation, "trainer");
-    printf("employee id:%d\nemployee name:%s\ndesgination:%s\n", e1.id, e1.name, e1.designation);
-    /*use of dot to seprate betwee var and attr*/
-    printf("------------------\n\n");                                                                  /*by variable*/
-    employee *ep = &e1;                                                                                /*pointer structure*/
-    printf("employee id: %d\nemployee name:%s\n`desgination:%s\n", ep->id, ep->name, ep->designation); /* by pointer */
+    identify id1;
+    strcpy(id1.drivinglicense, "DL XZL2002 ");
+    strcpy(id1.passport, "pp13435");
+    printf("size:%d\n", sizeof(id1));
 
+    printf("\n\n%s\n", id1.drivinglicense);
+    printf("\n\n%s\n", id1.passport);
     return 0;
 }
