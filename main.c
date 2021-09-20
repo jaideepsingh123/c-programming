31cf026ef3aba448f389f559faccc26393fa938d
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-20 10:58:52 +0530

    structure

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-//register storage class
-int main()
+#include <stdlib.h>
+#include <string.h>
+//structure provide provision of combining several types of data items under a single entity represent data
+//entity is a object use to identify units
+struct emp
 {
-    register int a = 100;
-    printf("value of a:%d\n\n", a);
+    int id;
+    char name[10];
+    char designation[10];
+};
+typedef struct emp employee;
+int main()
 
-    printf("address of a:%d\n\n", &a); /*this will not give any value because register var do not have address*/
+{
+    employee e1; /*e1 is even no  of variable to employee*/
+    e1.id = 100;
+    strcpy(e1.name, "jaideep");
+    strcpy(e1.designation, "trainer");
+    printf("employee id:%d\nemployee name:%s\ndesgination:%s\n", e1.id, e1.name, e1.designation);
+    /*use of dot to seprate betwee var and attr*/
+    printf("------------------\n\n");                                                                  /*by variable*/
+    employee *ep = &e1;                                                                                /*pointer structure*/
+    printf("employee id: %d\nemployee name:%s\n`desgination:%s\n", ep->id, ep->name, ep->designation); /* by pointer */
+
     return 0;
 }
