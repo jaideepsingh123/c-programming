36b7cbc69ddbd35009632e19cc0dac22292b3ebc
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-21 14:57:43 +0530

    file i/o

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//union is a data type enable to store different data type in the same memory location also provide efficent way of using same memory for multi-purpose
-union id
-{
-    char drivinglicense[10];
-    char passport[15];
-};
-typedef union id identify;
+//file i/o// is collection of charactors stored on a permanent storage device it
+/*standard file automatically opend when program is executes to provide acces to the keyboard (stdin)input,(stdout)(stdrerr)*/
 int main()
 {
-    identify id1;
-    strcpy(id1.drivinglicense, "DL XZL2002 ");
-    strcpy(id1.passport, "pp13435");
-    printf("size:%d\n", sizeof(id1));
-
-    printf("\n\n%s\n", id1.drivinglicense);
-    printf("\n\n%s\n", id1.passport);
+    //int c;
+    //  printf("Enter a char:");
+    //c = getchar();
+    //printf("\nyou entered:");
+    //putchar(c);
+    //char str[100];
+    //printf("\nenter a line:");
+    //gets(str);
+    //printf("\nyou entered:");
+    //puts(str);
+    FILE *fp;
+    fp = fopen("test.txt", "w+");
+    fclose(fp);
     return 0;
 }
