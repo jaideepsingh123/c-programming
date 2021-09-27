bc41eafad1a2b30837a2098754bc829fc57c00e6
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-27 10:19:51 +0530

    function-star

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-//function is a group of statement that together perform a task it also know as method and sub-routine or procedure main is function (function_name),return types(int,float,double,) what value we wnat to return _types fuction_name(data_typeparameters1(int num) ,data parameter2,....)
-void sum(int x, int y);
+int sum(int a, int b); /*function*/
+void printstar(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c", '*');
+    }
+
+    printf("%c", '*');
+}
+
 int main()
 {
-    int a = 15, b = 50;
-    sum(10, 30);
-    sum(a, b);
+    int a, b, c; /*declaration*/
+    a = 10;
+    b = 20;
+    c = sum(a, b);
+    printstar(7);
+    /*printf("the sum is %d\n", c);*/
     return 0;
 }
-void sum(int x, int y)
+
+int sum(int a, int b) /*function do not call after declaration*/
 {
-    int total = x + y;
-    printf("total :%d\n", total);
+    return a + b;
 }
