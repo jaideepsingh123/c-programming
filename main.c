7d8b352199deab3e128d7b46d3e9921beda3000a
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-29 12:23:49 +0530

    function call by value

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-int sum(int a, int b)
+//function call by value
+void swap(int x, int y)
 {
-    return a + b;
+    int temp;
+    temp = x; /*save the value of x*/
+    x = y;    /*put y into x */
+    y = temp; /*put temp into y*/
+    return;
 }
-
 int main()
 {
-    int a = 25, b = 25;
-    printf("total:%d", sum(a, b));
+    int a = 100;
+    int b = 200;
+    printf("before swap value of a:%d\n", a);
+    printf("before swap value of b:%d\n", b);
+    swap(a, b);
+    printf("after swap value of a:%d\n", a);
+    printf("after swap value of b:%d\n", b);
     return 0;
 }
