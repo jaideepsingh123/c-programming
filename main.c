8a98125ffe6c400791b6fa0f8adad1d029711a04
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-29 10:29:30 +0530

    function-addtion

--- main.c
+++ main.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
-int sum(int a, int b); /*function*/
-void printstar(int n)
+int sum(int a, int b)
 {
-    for (int i = 0; i < n; i++)
-    {
-        printf("%c", '*');
-    }
-
-    printf("%c", '*');
+    return a + b;
 }
 
 int main()
 {
-    int a, b, c; /*declaration*/
-    a = 10;
-    b = 20;
-    c = sum(a, b);
-    printstar(7);
-    /*printf("the sum is %d\n", c);*/
+    int a = 25, b = 25;
+    printf("total:%d", sum(a, b));
     return 0;
-}
-
-int sum(int a, int b) /*function do not call after declaration*/
-{
-    return a + b;
 }
