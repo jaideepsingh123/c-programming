28d21f26e1a867f5afe2bebe57d6d32eb1cb9a20
jaideepsingh123 <jai.kharbanda2002@gmail.com>
2021-09-30 10:03:04 +0530

    pointer and fuction

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-//function call by address is method of passing arugments to  a function copies the address of an arugment into formal parameters
-void swap(int *x, int *y)
+#include <stdlib.h>
+//pointer and function
+int *add(int *x, int *y)
 {
-    int temp;
-    temp = *x; /*save the value at address x*/
-    *x = *y;   /*put y into x*/
-    *y = temp; /*put temp into y*/
-    return;
+    int sum = *x + *y;
+
+    return &sum;
 }
 int main()
 {
     int a = 100;
-    int b = 500;
-    printf("before swap value of a :%d\n", a);
-    printf("before swap ,value of b:%d\n", b);
-    swap(&a, &b);
-    printf("after swap of a:%d\n", a);
-    printf("after swap value of b:%d\n", b);
+    int b = 200;
+    int *ptr = add(&a, &b);
+    printf("%d", *ptr);
     return 0;
 }
-//in this value is store at same place  rather  called by value there are multiples copy of value and there are not store at same place
